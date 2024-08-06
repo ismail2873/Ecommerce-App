4757ac605d418262417f11a83543a7c1c97b013b
ismail2873 <ismailniazi065@gmail.com>
2024-08-06 20:52:37 +0500

    initialized project

---
+++ lib/home/splash_screen/splash_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
+import 'package:app1/home/home_page/home_page.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'E-commerce App',
+      debugShowCheckedModeBanner: false,
+      home: FlutterSplashScreen.gif(
+        gifPath: 'assets/gifs/splash.gif',
+        gifWidth: 269,
+        gifHeight: 474,
+        nextScreen: const HomePage(),
+        duration: const Duration(milliseconds: 3515),
+        onInit: () async {
+          debugPrint("onInit");
+        },
+        onEnd: () async {
+          debugPrint("onEnd 1");
+        },
+      )
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'home/splash_screen/splash_screen.dart';
+
+void main() {
+  runApp(const SplashScreen());
+}
